7759572b22d218b93594d2a7978f8bd42bc2d071
shivani jha <shivanijha1507@gmail.com>
2022-02-08 18:15:10 +0530

    build-ready

--- src/Row.js
+++ src/Row.js
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
 import ScrollContainer from "react-indiana-drag-scroll";
-import YouTube from "react-youtube";
-import movieTrailer from "movie-trailer";
+
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("");
+  
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -16,25 +15,8 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }
     fetchData();
   }, [fetchUrl]);
-  const youtubeOpts = {
-    height: "390",
-    width: "100%",
-    playerVars: {
-      autoplay: 1,
-    },
-  };
-  const movieClicked = (moviename) => {
-    console.log(moviename);
-    if (trailerUrl != "") setTrailerUrl("");
-    else {
-      movieTrailer(moviename)
-        .then((url) => {
-          const urlParamV = new URLSearchParams(new URL(url).search);
-          setTrailerUrl(urlParamV.get("v"));
-        })
-        .catch((err) => console.log(err));
-    }
-  };
+  
+  
   return (
     <div className="row">
       <h2>{title}</h2>
