8821212342b0675f566bdc894ac916322280272b
Shivani-jha-768 <73032304+Shivani-jha-768@users.noreply.github.com>
2022-02-10 17:29:03 +0530

    Row js updated

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import RowCard from "./RowCard"
 import ScrollContainer from "react-indiana-drag-scroll";
 
 
@@ -23,15 +24,11 @@ function Row({ title, fetchUrl, isLargeRow }) {
       <ScrollContainer className="row__posters">
         {/* <div className="row__posters"> */}
         {movies.map((movie) => (
-          <img
-            
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`} //use && if theres no else or : otherwise use ?
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
+         
+          <RowCard  isLargeRow  base_url = {base_url} movie = {movie}/>
+          
         ))}
+        
         {/* </div> */}
       </ScrollContainer>
       
