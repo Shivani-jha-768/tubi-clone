af8be24caf7fb38d767d7ac71aeda2644a8ef43e
Shivani-jha-768 <73032304+Shivani-jha-768@users.noreply.github.com>
2022-02-10 17:28:13 +0530

    RowCard added

---
+++ src/RowCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './RowCard.css'
+function RowCard({ base_url, movie, isLargeRow}) {
+  return (
+    
+        <div>
+            <img
+                
+                className="row__posters row__posterLarge" //use && if theres no else or : otherwise use ?
+                src={`${base_url}${
+                {isLargeRow} ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+            />
+            
+            
+             {/* <div className="button"><img src="http://cdn1.iconfinder.com/data/icons/flavour/button_play_blue.png" alt = "Play"/> </div> */}
+             <h5 style={{paddingLeft:"10px"}}>{movie?.name || movie?.title || movie?.orginal_name}</h5>
+             <h6 style={{paddingLeft:"10px"}}>Action</h6>
+
+        </div>
+  
+  )
+}
+
+export default RowCard
